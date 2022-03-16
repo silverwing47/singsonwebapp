b5efd9f6c90cb108a9c6d483f779b0884115a90a
silverwing47 <90695743+silverwing47@users.noreply.github.com>
2022-03-17 00:02:38 +0800

    mao na jud ni

--- Singson/urls.py
+++ Singson/urls.py
@@ -18,5 +18,5 @@ from django.urls import include,path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('singsonWebApp/', include('singsonWebApp.urls', namespace='singsonWebApp')),
+    path('', include('singsonWebApp.urls', namespace='singsonWebApp')),
 ]
